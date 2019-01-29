4ee9b1160e5d8388e08b8adf3f711f3b08456015
jaminchen <oopsplus@vip.qq.com>
2019-01-29 15:59:36 +0000

    add users create page

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('/testvue', 'StaticPagesController@testvue');
 
+//注册路由时，URI signup 和 /signup 从使用上来看，并无区别，Laravel 框架兼容这两种写法。
+Route::get('/signup', 'UsersController@create')->name('signup');
