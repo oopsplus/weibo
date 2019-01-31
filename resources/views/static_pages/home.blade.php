4185f4d1b3771424b1c645d7203abf2f7a2683b4
jaminchen <oopsplus@vip.qq.com>
2019-01-31 19:09:19 +0800

    post status

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.default')
 @section('content')
+  @if (Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+      </div>
+    </div>
+  @else
   <div class="jumbotron">
     <h1>Hello Laravel</h1>
     <p class="lead">
@@ -12,4 +21,5 @@
       <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
     </p>
   </div>
+  @endif
 @stop
