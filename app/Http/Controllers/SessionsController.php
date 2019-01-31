2f01b9838a3b49f97af040aa5a5e597b50dc3036
jaminchen <oopsplus@vip.qq.com>
2019-01-31 11:21:46 +0800

    remember me on login

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -18,7 +18,7 @@ class SessionsController extends Controller
        ]);
        //如果validate失败，则old('email')会被自动填充
 
-       if (Auth::attempt($credentials)) {
+       if (Auth::attempt($credentials, $request->has('remember'))) {
            session()->flash('success', '欢迎回来！');
            return redirect()->route('users.show', [Auth::user()]);
        } else {
